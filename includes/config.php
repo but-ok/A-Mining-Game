65dc6d80ed65eecd3a9fc9093fa7a10bb6fa9d54
but-ok <89722368+but-ok@users.noreply.github.com>
2021-08-29 17:05:28 +0930

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 //database
-$db_host = 'localhost';
+$db_host = '184.168.98.214';
 $db_name = 'agame';
-$db_user = 'root';
-$db_password = '';
+$db_user = 'biggamer';
+$db_password = 'Halo23!';
 
 ?>
